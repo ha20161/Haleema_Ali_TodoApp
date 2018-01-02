eecf44174c6b4b51646e0bd6ea549b5f3e330372
Haleema Ali <Haleema@Haleemas-MacBook-Pro.local>
2018-01-02 21:58:53 +0000

    nav bar complete

--- app/src/main/java/com/example/haleema_ali_todoapp/TodoListActivity.java
+++ app/src/main/java/com/example/haleema_ali_todoapp/TodoListActivity.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
 
 public class TodoListActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -14,6 +21,14 @@ public class TodoListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fragment);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
@@ -26,7 +41,16 @@ public class TodoListActivity extends AppCompatActivity {
             // a fragment is inserted into FrameLayout view object of TodoListActivity
             fm.beginTransaction().add(R.id.fragment_container, todoListFragment).commit();
         }
-
-
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
